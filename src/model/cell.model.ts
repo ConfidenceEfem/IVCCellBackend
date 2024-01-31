ddd40eec23620b145bad1ae04b14805555200862
unknown <confidenceefem1@gmail.com>
2024-01-31 02:42:27 +0100

    first commit

--- src/model/cell.model.ts
+++ src/model/cell.model.ts
@@ -8,20 +8,23 @@ const cellSchema = new Schema<Icell>(
     cellEmail: { type: String, required: true },
     image: {
       type: String,
+      default:""
     },
    
     maritalStatus: {
       type: String,
+      default:""
     },
     bio: {
       type: String,
+      default:""
     },
     cellAdminId: {
       type: Schema.Types.ObjectId,
       ref: "admin",
     },
     cellPassword: { type: String },
-    cellLeaderName: { type: String },
+    cellLeaderName: { type: String,default:"" },
     isEmailVerified: { type: Boolean, default: false },
     isSuspended: { type: Boolean, default: false },
     cellReport: [
